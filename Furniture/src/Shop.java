be9271c3bc421c5646c7b7eea77c2a5873551cf5
Andrii Krenevych <krenevych@knu.ua>
2024-04-18 11:11:38 +0300

    Умова фікс

--- Furniture/src/Shop.java
+++ Furniture/src/Shop.java
@@ -2,21 +2,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Shop {
-    public enum Type {
+
+    public enum Type {  // Типи меблів, що може надавати
         TABLE,
         CHAIR,
     }
+
+    // Сировина для виготовлення меблів
+    private String material = "Wood";  // матеріал
+    private String color = "Crimson";  // фарба
+    private int nails = 100;           // цвяхи, шурупи тощо
 
     // Cписок стільців, що є в наявності в магазині
     private final List<Chair> chairs = new ArrayList<>();
 
     // Cписок столів, що є в наявності в магазині
     private final List<Table> tables = new ArrayList<>();
-
-    // Сировина для виготовлення столів
-    protected String material = "Wood";  // матеріал
-    protected String color = "Crimson";  // фарба
-    protected int nailNumber = 100;      // цвяхи, шурупи тощо
 
     // Демонструвати меблі в салоні
     void demonstrateFurniture(){
@@ -27,10 +28,10 @@ public class Shop {
     // Виготовити нові меблі за типом
     void createFurniture(Type type){
         if (type == Type.CHAIR){
-            Chair chair = new Chair(material, color, nailNumber);
+            Chair chair = new Chair(material, color, nails);
             chairs.add(chair);
         }else if (type == Type.TABLE){
-            Table table = new Table(material, color, nailNumber);
+            Table table = new Table(material, color, nails);
             tables.add(table);
         }
     }
